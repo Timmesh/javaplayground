ca2f468897bcab0f34e720257b87ec2350a0da8d
Timmesh Kurmayya <ktimmesh67@gmail.com>
2020-02-13 02:41:21 +0530

    Using Form Data and Resetting Form

--- Angular/UdemyCourse/forms-td-start/src/app/app.component.ts
+++ Angular/UdemyCourse/forms-td-start/src/app/app.component.ts
@@ -7,29 +7,47 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f', { static: false }) signUpForm: NgForm;
+  @ViewChild('f', { static: false }) signupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
   genders = ['Male', 'Female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
-    // this.signUpForm.setValue({
+    // this.signupForm.setValue({
     //   userData: {
     //     username: suggestedName,
     //     email: ''
     //   },
     //   secret: 'pet',
     //   questionAnswer: '',
-    //   gender: ''
+    //   gender: 'male'
     // });
-    this.signUpForm.form.patchValue({
+    this.signupForm.form.patchValue({
       userData: {
         username: suggestedName
       }
     });
   }
 
+
   onSubmit() {
-    console.log(this.signUpForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
+
   }
 }
