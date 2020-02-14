46b779b87edb0bf48d0ab0566e87be1211108105
Timmesh Kurmayya <ktimmesh67@gmail.com>
2020-02-15 03:20:02 +0530

    Showing a loading indicator

--- Angular/UdemyCourse/http-01-start/src/app/app.component.ts
+++ Angular/UdemyCourse/http-01-start/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) { }
 
@@ -31,6 +32,7 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>(
         'https://ng-complete-guide-c56d3.firebaseio.com/posts.json'
@@ -47,6 +49,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe(posts => {
+        this.isFetching = false;
         this.loadedPosts = posts;
       });
   }
