ea333e6bf7b29c5e633737e66db11aeb0cbc1869
Timmesh Kurmayya <ktimmesh67@gmail.com>
2021-04-19 18:23:19 +0530

    Added Comments and mocked subscribe method

--- Angular/AngularUnitTestingCourse-PS/src/app/heroes/heroes.component.spec.ts
+++ Angular/AngularUnitTestingCourse-PS/src/app/heroes/heroes.component.spec.ts
@@ -1,6 +1,5 @@
-import { of } from "rxjs";
-import { HeroesComponent } from "./heroes.component";
-// import { of } from "rxjs/observable/of";
+import { HeroesComponent } from './heroes.component';
+import { of } from 'rxjs';
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
@@ -9,34 +8,49 @@ describe('HeroesComponent', () => {
 
   beforeEach(() => {
     HEROES = [
-      {id:1, name: 'SpiderDude', strength: 8},
-      {id:2, name: 'Wonderful Woman', strength: 24},
-      {id:3, name: 'SuperDude', strength: 55}
-    ]
+      { id: 1, name: 'Spider Dude', strength: 8 },
+      { id: 2, name: 'Wonderful Woman', strength: 24 },
+      { id: 3, name: 'Super Dude', strength: 55 }
+    ];
 
-    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero'])
+    // creates mock hero service with methods provided in string array
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
 
     component = new HeroesComponent(mockHeroService);
-  })
+  });
 
+  // state test
   describe('delete', () => {
-
     it('should remove the indicated hero from the heroes list', () => {
-      mockHeroService.deleteHero.and.returnValue(of(true))
+      // delete method calls subscribe on the the hero service deteleHero method, so need to mock that and its result out
+      mockHeroService.deleteHero.and.returnValue(of(true));
       component.heroes = HEROES;
 
       component.delete(HEROES[2]);
 
-      expect(component.heroes.length).toBe(2);
-    })
+      expect(component.heroes.find(hero => hero.id === HEROES[2].id)).toBeUndefined();
+      expect(component.heroes.find(hero => hero.id === HEROES[0].id)).not.toBeUndefined();
+      expect(component.heroes.find(hero => hero.id === HEROES[1].id)).not.toBeUndefined();
+    });
 
-    it('should call deleteHero', () => {
-      mockHeroService.deleteHero.and.returnValue(of(true))
+    // interaction test, asserts that external call is made with expected parameters passed
+    it('should call deleteHero with correct hero', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
       component.heroes = HEROES;
 
       component.delete(HEROES[2]);
 
       expect(mockHeroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
-    })
-  })
-})
+    });
+
+    it('should call subscribe on result of deleteHero', () => {
+    const mockDeleteHeroMethod = jasmine.createSpyObj(['subscribe']);
+    mockHeroService.deleteHero.and.returnValue(mockDeleteHeroMethod);
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(mockDeleteHeroMethod.subscribe).toHaveBeenCalledWith();
+    });
+  });
+});
