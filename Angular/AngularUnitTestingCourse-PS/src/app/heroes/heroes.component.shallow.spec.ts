5cd90492dfc93192eb6b17159a12a83f5baf8775
Timmesh Kurmayya <ktimmesh67@gmail.com>
2021-04-20 08:30:32 +0530

    Mocking an injected service

---
+++ Angular/AngularUnitTestingCourse-PS/src/app/heroes/heroes.component.shallow.spec.ts
@@ -0,0 +1,45 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from '../hero.service';
+import { of } from 'rxjs';
+import { Component, Input, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Hero } from '../hero';
+import { By } from '@angular/platform-browser';
+
+describe('HeroesComponent (shallow tests)', () => {
+  let fixture: ComponentFixture<HeroesComponent>;
+  let mockHeroService;
+  let HEROES;
+
+  beforeEach(() => {
+    HEROES = [
+      { id: 1, name: 'Spider Dude', strength: 8 },
+      { id: 2, name: 'Wonderful Woman', strength: 24 },
+      { id: 3, name: 'Super Dude', strength: 55 }
+    ];
+
+    // Mocking injected service in component
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+
+    TestBed.configureTestingModule({
+      declarations: [
+        HeroesComponent
+      ],
+      providers: [
+        { provide: HeroService, useValue: mockHeroService }
+      ],
+      schemas: [NO_ERRORS_SCHEMA] 
+      
+    });
+
+    fixture = TestBed.createComponent(HeroesComponent);
+  });
+
+  it('should set heroes correctly from the service', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+
+    expect(fixture.componentInstance.heroes.length).toBe(3);
+  });
+
+});
