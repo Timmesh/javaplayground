0d2bc51dcc6332ef9f2d9225206044e18e14fcc5
Timmesh Kurmayya <ktimmesh67@gmail.com>
2021-04-20 09:09:29 +0530

    Creating a Deep Integration Test

---
+++ Angular/AngularUnitTestingCourse-PS/src/app/heroes/heroes.component.deep.spec.ts
@@ -0,0 +1,44 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from '../hero.service';
+import { HeroComponent } from '../hero/hero.component';
+import { NO_ERRORS_SCHEMA, Directive, Input } from '@angular/core';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+
+describe('HeroesComponent (deep tests)', () => {
+  let fixture: ComponentFixture<HeroesComponent>;
+  let mockHeroService;
+  let HEROES;
+
+  beforeEach(() => {
+    HEROES = [
+      { id: 1, name: 'Spider Dude', strength: 8 },
+      { id: 2, name: 'Wonderful Woman', strength: 24 },
+      { id: 3, name: 'Super Dude', strength: 55 }
+    ];
+
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+
+    TestBed.configureTestingModule({
+      declarations: [
+        HeroesComponent,
+        HeroComponent,
+      ],
+      providers: [
+        { provide: HeroService, useValue: mockHeroService }
+      ],
+      // schemas: [NO_ERRORS_SCHEMA] // this is to work with the issue of the missing app routing module
+    });
+
+    fixture = TestBed.createComponent(HeroesComponent);
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    // this fixture only points at the HeroesComponent, not the child HeroComponent, 
+    // but because we have called change detection on the parent component, 
+    // change detection will then run on all child components. 
+    // So not only will the parent component get initialized, but all child components will get initialized.
+    fixture.detectChanges();
+  });
+
+  
+});
