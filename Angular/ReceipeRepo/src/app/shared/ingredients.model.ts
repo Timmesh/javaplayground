bb3d16c3c6bde5bd35ea9ab3bc55f6e2ba616bdc
Timmesh Kurmayya <ktimmesh67@gmail.com>
2021-05-26 08:56:26 +0530

    Displaying Shopping list

---
+++ Angular/ReceipeRepo/src/app/shared/ingredients.model.ts
@@ -0,0 +1,9 @@
+export class Ingredient {
+
+  /**
+   * This is similar to adding name and amount as attribte.
+   * @param name , not null
+   * @param amount  not null
+   */
+  constructor(public name: string, public amount: number) {}
+}
