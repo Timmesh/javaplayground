1326265e66d1832899598fcc74c6976e8d3f5ead
Timmesh Kurmayya <ktimmesh67@gmail.com>
2021-05-27 01:31:24 +0530

    Passing Data with Event and Property Binding

--- Angular/ReceipeRepo/src/app/receipes/receipe-list/receipe-item/receipe-item.component.ts
+++ Angular/ReceipeRepo/src/app/receipes/receipe-list/receipe-item/receipe-item.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Receipe } from '../../receipe.model';
 
 @Component({
   selector: 'app-receipe-item',
   templateUrl: './receipe-item.component.html',
-  styleUrls: ['./receipe-item.component.css']
+  styleUrls: ['./receipe-item.component.css'],
 })
 export class ReceipeItemComponent implements OnInit {
+  @Input() receipe: Receipe;
+  @Output() receipeClicked: EventEmitter<void> = new EventEmitter<void>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSelected() {
+    this.receipeClicked.emit();
   }
-
 }
--- Angular/ReceipeRepo/src/app/receipes/receipe-list/receipe-list.component.ts
+++ Angular/ReceipeRepo/src/app/receipes/receipe-list/receipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Receipe } from '../receipe.model';
 
 @Component({
@@ -7,18 +7,25 @@ import { Receipe } from '../receipe.model';
   styleUrls: ['./receipe-list.component.css'],
 })
 export class ReceipeListComponent implements OnInit {
+  @Output() receipeSelected: EventEmitter<Receipe> =
+    new EventEmitter<Receipe>();
+
   receipes: Receipe[] = [
     new Receipe(
-      'A Test Recipe',
+      'A Test Recipe1',
       'This is simply a test',
       'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
     ),
     new Receipe(
-      'A Test Recipe',
+      'A Test Recipe2',
       'This is simply a test',
       'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
     ),
   ];
+
+  onReceipeSelected(receipe: Receipe) {
+    this.receipeSelected.emit(receipe);
+  }
 
   constructor() {}
 
