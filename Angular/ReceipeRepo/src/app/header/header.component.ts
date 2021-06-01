f9943602731547f4e6ec7900e1223be892ff2683
Timmesh Kurmayya <ktimmesh67@gmail.com>
2021-06-01 09:28:48 +0530

    Adding Navigation to the App

--- Angular/ReceipeRepo/src/app/header/header.component.ts
+++ Angular/ReceipeRepo/src/app/header/header.component.ts
@@ -1,15 +1,10 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: "app-header",
-  templateUrl: "./header.component.html",
+  selector: 'app-header',
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent {
   collapsed = true;
-  @Output()
-  featureSelected: EventEmitter<string> = new EventEmitter<string>();
 
-  onselect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
 }
