420742bbe5b32ebc0a4eefd57e3a8184430baddd
Timmesh Kurmayya <ktimmesh67@gmail.com>
2021-06-02 22:12:59 +0530

    Adding custom Observable

--- Angular/UdemyCourse/obs-01-start/src/app/home/home.component.ts
+++ Angular/UdemyCourse/obs-01-start/src/app/home/home.component.ts
@@ -1,24 +1,34 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import {interval, Subscription } from 'rxjs'
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { interval, Observable, Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit,OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy {
   private firstObsSubscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe(count => {
+    /* this.firstObsSubscription = interval(1000).subscribe(count => {
       console.log(count);
+    }); */
+    // Custom Observable
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
     });
+    this.firstObsSubscription = customIntervalObservable.subscribe((count) =>
+      console.log(count)
+    );
   }
 
   ngOnDestroy() {
     this.firstObsSubscription.unsubscribe();
   }
-
 }
