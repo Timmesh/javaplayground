6f2f3a32786f2c94c436f793d95016e86bb45d38
Timmesh Kurmayya <ktimmesh67@gmail.com>
2021-06-05 18:21:15 +0530

    Receipe Edit form using Reactive approach

--- Angular/ReceipeRepo/src/app/receipes/receipe-edit/receipe-edit.component.ts
+++ Angular/ReceipeRepo/src/app/receipes/receipe-edit/receipe-edit.component.ts
@@ -1,23 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+import { ActivatedRoute, Params } from "@angular/router";
+import { ReceipeService } from "../receipe.service";
 
 @Component({
-  selector: 'app-receipe-edit',
-  templateUrl: './receipe-edit.component.html',
-  styleUrls: ['./receipe-edit.component.css']
+  selector: "app-receipe-edit",
+  templateUrl: "./receipe-edit.component.html",
+  styleUrls: ["./receipe-edit.component.css"],
 })
 export class ReceipeEditComponent implements OnInit {
   id: number;
   editMode: boolean;
-  constructor(private route: ActivatedRoute) { }
+  receipeForm: FormGroup;
+
+  constructor(
+    private route: ActivatedRoute,
+    private receipeService: ReceipeService
+  ) {}
 
   ngOnInit() {
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.id = +params['id'];
-        this.editMode = params['id'] != null;
-      }
-    );
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params["id"];
+      this.editMode = params["id"] != null;
+      this.initForm();
+    });
   }
 
+  private initForm() {
+    let receipeName = "";
+    let receipeImagePath = "";
+    let receipeDescription: string = "";
+
+    if (this.editMode) {
+      const receipe = this.receipeService.getReceipe(this.id);
+      receipeName = receipe.name;
+      receipeImagePath = receipe.imagePath;
+      receipeDescription = receipe.description;
+    }
+    this.receipeForm = new FormGroup({
+      'name': new FormControl(receipeName),
+      'imagePath': new FormControl(receipeImagePath),
+      'description': new FormControl(receipeDescription),
+    });
+  }
+
+  onSubmit() {
+    console.log("This");
+    
+    console.log(this.receipeForm);
+    
+  }
 }
