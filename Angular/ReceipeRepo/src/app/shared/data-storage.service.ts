6b52594f195492e62bc6a4dbc61f0cbcf8aa4d4d
Timmesh Kurmayya <ktimmesh67@gmail.com>
2021-06-30 12:19:27 +0530

    Adding token to outgoing request

--- Angular/ReceipeRepo/src/app/shared/data-storage.service.ts
+++ Angular/ReceipeRepo/src/app/shared/data-storage.service.ts
@@ -1,13 +1,15 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Receipe } from "../receipes/receipe.model";
 import { ReceipeService } from "../receipes/receipe.service";
-import { map, tap } from "rxjs/operators";
+import { exhaustMap, map, take, tap } from "rxjs/operators";
+import { AuthService } from "../auth/auth.service";
 @Injectable({ providedIn: "root" })
 export class DataStorageService {
   constructor(
     private httpClient: HttpClient,
-    private receipeService: ReceipeService
+    private receipeService: ReceipeService,
+    private authService: AuthService
   ) {}
 
   storeReceipes() {
@@ -21,22 +23,27 @@ export class DataStorageService {
   }
 
   fetchReceipes() {
-    return this.httpClient
-      .get<Receipe[]>(
-        "https://receiperepo-default-rtdb.firebaseio.com/receipe.json"
-      )
-      .pipe(
-        map((receipes) => {
-          return receipes.map((receipe) => {
-            return {
-              ...receipe,
-              ingredients: receipe.ingredients ? receipe.ingredients : [],
-            };
-          });
-        }),
-        tap((receipes) => {
-          this.receipeService.setRecepies(receipes);
-        })
-      );
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        return this.httpClient.get<Receipe[]>(
+          "https://receiperepo-default-rtdb.firebaseio.com/receipe.json",
+          {
+            params: new HttpParams().set('auth', user.token)
+          }
+        );
+      }),
+      map((receipes) => {
+        return receipes.map((receipe) => {
+          return {
+            ...receipe,
+            ingredients: receipe.ingredients ? receipe.ingredients : [],
+          };
+        });
+      }),
+      tap((receipes) => {
+        this.receipeService.setRecepies(receipes);
+      })
+    );
   }
 }
